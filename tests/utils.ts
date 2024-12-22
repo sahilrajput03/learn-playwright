4989e7ff288dcff7bdad9ac522c80f8008dca7fd
Sahil Rajput <sahilrajput03@gmail.com>
2024-12-22 11:42:41 +0530

    Update.

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,12 +1,7 @@
 import path from 'path';
 
 export const githubStorageStateFile = path.join(__dirname, './storage-state/.auth/github-user.json');
-// console.log("🚀 ~ githubStorageStateFile:", githubStorageStateFile)
-// OUTPUT: "/Users/apple/Documents/learn-playwright/tests/storage-state/.auth/user.json"
-
 export const awsStorageStateFile = path.join(__dirname, './storage-state/.auth/aws-user.json');
-// console.log("🚀 ~ awsStorageStateFile:", awsStorageStateFile)
-// OUTPUT: "/Users/apple/Documents/learn-playwright/tests/storage-state/.auth/aws-user.json"
 
 export const awsLoginUrl = 'https://ap-south-1.console.aws.amazon.com'
 export const githubLoginUrl = 'https://github.com/login'
